7262add66ff26d1feae31cc3457721a276026a7c
Nielen Seldte <129792303+nielenseldte@users.noreply.github.com>
2025-08-05 16:59:57 +0200

    replaced eval() with operate method
    calculator obeys the rules of PEDMAS by calculating once two operands are entered

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
     let elements = document.getElementsByTagName("li");
     let screen = document.querySelectorAll("p")[0];
     let clear = document.getElementsByClassName('clear')[0];
+    let operator = null;
+    let operands = [];
 
     function addToCurrentValue(i) {
         return function () {
@@ -9,49 +11,84 @@
             switch (value) {
                 case "\u00F7":
                     screen.innerText += "/";
+                    operator = "/"
                     break;
                 case "\u00D7":
                     screen.innerText += "*";
+                    operator = "*";
                     break;
                 case "\u2212":
                     screen.innerText += "-";
+                    operator = "-";
+                    break;
+                case "+":
+                    screen.innerText += "+";
+                    operator = "+";
                     break;
                 default:
                     screen.innerText += value;
+                    operands.push(Number(value));
+            }
+            if (operands.length === 2) {
+                calculate()();
             }
         };
     }
 
     for (let i = 0; i < elements.length; i++) {
-        if (elements[i].innerHTML === "=") {
+        if (elements[i].innerText === "=") {
             elements[i].addEventListener("click", calculate(i));
         } else {
             elements[i].addEventListener("click", addToCurrentValue(i));
         }
     }
 
-    clear.onclick = function () {
-        screen.innerHTML = "";
-    };
+    clear.addEventListener('click', function (e) {
+        clearScreen();
+        operands = [];
+        operator = null;
+    });
+
+    function clearState() {
+        operands = [];
+        operator = null;
+    }
+
+    function clearScreen() {
+        screen.innerText = '';
+    }
 
     function calculate() {
         return function () {
-            screen.innerText = eval(screen.innerText);
+            if (operands.length === 1) {
+                answer = operands[0];
+            } else {
+                answer = operate(operator, operands[0], operands[1]);
+            }
+            screen.innerText = answer;
+            if (answer === 'undefined') {
+                clearState();
+                setTimeout(() => {
+                    clearScreen();
+                }, 500);
+            } else {
+                clearState();
+                operands.push(answer);
+            }
         };
     }
 
-    //we need to eventually switch to this, eval is dangerous
-    // function operate(operator, number1, number2) {
-    //     const operations = {
-    //         "+": (a, b) => a + b,
-    //         "-": (a, b) => a - b,
-    //         "*": (a, b) => a * b,
-    //         "/": (a, b) => b === 0 ? "Seriously, Division by zero is undefined" : a / b
-    //     }
-    //     const operation = operations[operator];
+    function operate(operator, number1, number2) {
+        const operations = {
+            "+": (a, b) => a + b,
+            "-": (a, b) => a - b,
+            "*": (a, b) => a * b,
+            "/": (a, b) => b === 0 ? "undefined" : a / b
+        }
+        const operation = operations[operator];
 
-    //     return operation ? operation(number1, number2) : "Operator not found";
-    // }
-
+        return operation ? operation(number1, number2) : "Operator not found";
+    }
 })();
+
 
